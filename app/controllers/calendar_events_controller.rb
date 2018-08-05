830d303266eb6b44aa15446b072eb43631035190
Fernando Guillen <fguillen.mail@gmail.com>
2018-08-06 00:16:08 +0200

    Adding Bootstrap

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,5 +1,5 @@
 class CalendarEventsController < ApplicationController
   def index
-    @calendar_events = CalendarEvent.all
+    @calendar_events_by_date = CalendarEvent.all.group_by { |calendar_event| calendar_event.date_time.to_date }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ Venue.create!(
   :scraper_module_name => "Dock11",
   :calendar_url => "http://example.com"
 )
+
+Venue.create!(
+  :name => "Villa Kuriosum",
+  :scraper_module_name => "VillaKuriosum",
+  :calendar_url => "http://example.com"
+)
