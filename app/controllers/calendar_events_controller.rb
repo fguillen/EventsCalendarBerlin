b01d60ef573eb957a6ad6f2a8fbd92894ccc73f9
Fernando Guillen <fguillen.mail@gmail.com>
2018-08-11 12:49:28 +0200

    Implementing bricklayer in CalendarEvents view

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,5 +1,12 @@
 class CalendarEventsController < ApplicationController
   def index
     @calendar_events_by_date = CalendarEvent.all.group_by { |calendar_event| calendar_event.date_time.to_date }
+    # @calendar_events_by_date = [[Time.now, CalendarEvent.all]]
+  end
+
+  def index_back
+    # @calendar_events_by_date = CalendarEvent.all.group_by { |calendar_event| calendar_event.date_time.to_date }
+    # @calendar_events_by_date = [[Time.now, CalendarEvent.all]]
+    # @calendar_events_by_date = [[Time.now, CalendarEvent.all]]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :calendar_events, :only => [:index]
+  resources :calendar_events, :only => [:index] do
+    get :index_back, :on => :collection
+  end
 end
