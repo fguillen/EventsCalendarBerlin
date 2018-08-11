c5c958103f537d0818f7a478e60dcee16eb07879
Fernando Guillen <fguillen.mail@gmail.com>
2018-08-11 14:19:51 +0200

    Cleaning

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,12 +1,5 @@
 class CalendarEventsController < ApplicationController
   def index
     @calendar_events_by_date = CalendarEvent.around_today.group_by { |calendar_event| calendar_event.date_time.to_date }
-    # @calendar_events_by_date = [[Time.now, CalendarEvent.all]]
-  end
-
-  def index_back
-    # @calendar_events_by_date = CalendarEvent.all.group_by { |calendar_event| calendar_event.date_time.to_date }
-    # @calendar_events_by_date = [[Time.now, CalendarEvent.all]]
-    # @calendar_events_by_date = [[Time.now, CalendarEvent.all]]
   end
 end
