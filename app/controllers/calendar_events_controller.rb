b3856c9a79262b0f2f17f0c8245f9775ffc0c868
Fernando Guillen <fguillen.mail@gmail.com>
2018-08-11 14:23:14 +0200

    Sorting by date

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,5 +1,5 @@
 class CalendarEventsController < ApplicationController
   def index
-    @calendar_events_by_date = CalendarEvent.around_today.group_by { |calendar_event| calendar_event.date_time.to_date }
+    @calendar_events_by_date = CalendarEvent.around_today.by_date.group_by { |calendar_event| calendar_event.date_time.to_date }
   end
 end
--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -2,6 +2,7 @@ class CalendarEvent < ApplicationRecord
   belongs_to :venue
 
   scope :around_today, -> { where("date_time between ? and ?", 1.day.ago, 30.days.from_now)}
+  scope :by_date, -> { order(:date_time => :asc) }
 
   serialize :info, JSON
   serialize :pics, JSON
