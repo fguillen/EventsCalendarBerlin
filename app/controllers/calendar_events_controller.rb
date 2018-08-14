fb023e72624bab5e18af74f942b15903e14ad3eb
Fernando Guillen <fguillen.mail@gmail.com>
2018-08-14 22:44:51 +0200

    Navbar

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -1,5 +1,8 @@
 class CalendarEventsController < ApplicationController
   def index
+    @dates = CalendarEvent.around_today.pluck("date(date_time)").uniq.sort
+    @venue_names = CalendarEvent.around_today.map { |e| e.venue.name }.uniq.sort
+    @tags = CalendarEvent.around_today.pluck(:tags).map { |e| e.split(",") }.flatten.uniq.sort
     @calendar_events_by_date = CalendarEvent.around_today.by_date.group_by { |calendar_event| calendar_event.date_time.to_date }
   end
 end
